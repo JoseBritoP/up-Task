35663fb44207e8d6531ae250f08530c90acac2e1
José Brito <josealbritose@gmail.com>
2024-04-09 08:47:49 -0300

    task types

--- api/src/typescript/types/task.d.ts
+++ api/src/typescript/types/task.d.ts
@@ -1,18 +1,10 @@
 import { Document,Types } from "mongoose";
-
+import { taskStatus } from "../../models/Task";
 
 export type TaskType = Document & {
   name:string
   description:string
   project: Types.ObjectId
 }
-
-const taskStatus = {
-  PENDING :'pending',
-  ON_HOLD :'onHold',
-  IN_PROGRESS:'inProgress',
-  UNDER_REVIEW:'underReview',
-  COMPLETED:'completed'
-} as const
 
 export type TaskStatus = typeof taskStatus[keyof typeof taskStatus]
