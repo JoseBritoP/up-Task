6013e20cf9d75c17e9f723d3f49807df4d749ea0
José Brito <josealbritose@gmail.com>
2024-04-09 08:54:26 -0300

    Format

--- api/src/controllers/task/01 - createTask.ts
+++ api/src/controllers/task/01 - createTask.ts
@@ -4,12 +4,14 @@ import { getProject } from "../project";
 
 export const createTask = async (data:unknown) => {
   const result = taskSchema.safeParse(data);
-  
+
   if(!result.success) throw new Error(JSON.stringify(result.error))
   
-  await getProject(result.data.project);
+  const project = await getProject(result.data.project);
 
   const newTask = new Task(result.data);
+  project.tasks.push(newTask.id)
+  await project.save();
   const savedTask = await newTask.save();
 
   return {
