874475c8035676b5bcfd848d7e7b06b2a2f008f8
José Brito <josealbritose@gmail.com>
2024-04-09 12:38:56 -0300

    updateMiddleware

--- api/src/middleware/task.ts
+++ api/src/middleware/task.ts
@@ -1,10 +1,13 @@
 import type { Request,Response,NextFunction } from "express";
 import { getProject } from "../controllers/project";
 import { idSchema } from "../schema/project";
+import { updateTaskSchema } from "../schema/task";
+import { UpdateTaskProps } from "../typescript/interfaces/task";
 
 declare global {
   namespace Express {
     interface Request {
+      taskUpdateData:UpdateTaskProps
       params:{
         projectId:string
       }
@@ -35,3 +38,16 @@ export const checkId = (req:Request,res:Response,next:NextFunction) => {
     return res.status(400).json({error:JSON.parse(error.message)})
   }
 }
+
+export const putTaskCheck = (req:Request,res:Response,next:NextFunction) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const result = updateTaskSchema.safeParse({id,data});
+    if(!result.success) throw new Error(JSON.stringify(result.error));
+    req.taskUpdateData = result.data
+    next();
+  } catch (error:any) {
+    return res.status(400).json({error:JSON.parse(error.message)});
+  }
+};
--- api/src/routes/taskRoutes.ts
+++ api/src/routes/taskRoutes.ts
@@ -4,7 +4,7 @@ import { TaskHandler } from "../handlers/tasks";
 const taskRouter = Router();
 // Middleware
 
-import { GetProjectId } from "../middleware/task";
+import { GetProjectId, putTaskCheck } from "../middleware/task";
 import { checkId } from "../middleware/task";
 
 // Endpoints
@@ -13,7 +13,7 @@ taskRouter.get('/',TaskHandler.GET)
 taskRouter.get('/project/:projectId',GetProjectId,TaskHandler.GET)
 taskRouter.get('/:id',checkId,TaskHandler.GETBYID)
 taskRouter.post('/',TaskHandler.POST)
-taskRouter.put('/:id',TaskHandler.PUT)
+taskRouter.put('/:id',putTaskCheck,TaskHandler.PUT)
 taskRouter.patch('/',TaskHandler.PATCH)
 taskRouter.delete('/',TaskHandler.DELETE)
 
