fd6dc2addfc8017c65f7c82e3545640bd271c0d2
José Brito <josealbritose@gmail.com>
2024-04-10 18:38:31 -0300

    ProjectCardContainer

--- client/src/components/Card/ProjectCardContainer.tsx
+++ client/src/components/Card/ProjectCardContainer.tsx
@@ -7,7 +7,7 @@ export default function ProjectCardContainer({data}:ProjectCardContainerProps) {
     <ul role="list" className="divide-y divide-gray-100 border border-gray-100 mt-10 bg-white shadow-lg rounded-md
     dark:bg-slate-900 dark:divide-gray-600 dark:border-gray-600 
     ">
-    {data.map((project) => (<ProjectCard project={project}/>))}
+    {data.map((project) => (<ProjectCard project={project} key={project._id}/>))}
     </ul>
   )
 }
