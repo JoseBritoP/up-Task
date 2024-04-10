3641e3d9430cbb434b0dafe105f8d3f9f6167624
José Brito <josealbritose@gmail.com>
2024-04-10 16:53:07 -0300

    interfaces

--- client/src/typescript/interfaces/Project.d.ts
+++ client/src/typescript/interfaces/Project.d.ts
@@ -1,6 +1,14 @@
-import { ProjectFormData } from "typescript/types/Project"
+import { Project, ProjectFormData } from "typescript/types/Project"
 
 export interface ProjectFormProps {
   register:UseFormRegister<ProjectFormData>
   errors:FieldErrors<ProjectFormData>
+}
+
+export interface ProjectCardContainerProps {
+  data:Project[]
+}
+
+export interface ProjectCardProps {
+  project:Project
 }
