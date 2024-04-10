5220067a9496c17b88b42b024ebe86536929e402
José Brito <josealbritose@gmail.com>
2024-04-10 16:52:52 -0300

    Project rendering

---
+++ client/src/components/Card/ProjectCard.tsx
@@ -0,0 +1,40 @@
+import { ProjectCardProps } from 'typescript/interfaces/Project'
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
+import { Link } from 'react-router-dom'
+
+export default function ProjectCard({project}:ProjectCardProps) {
+  return (
+    <li key={project._id} className="flex justify-between gap-x-6 px-5 py-10">
+      <div className="flex min-w-0 gap-x-4">
+        <div className="min-w-0 flex-auto space-y-2">
+          <Link to={`/project/${project._id}`} className="text-gray-600 dark:text-gray-300 cursor-pointer hover:underline text-3xl font-bold">{project.projectName}</Link>
+          <p className="text-sm text-gray-400">Client: <span>{project.clientName}</span></p>
+          <p className="text-sm text-gray-400">{project.description}</p>
+        </div>
+      </div>
+      <div className="flex shrink-0 items-center gap-x-6">
+        <Menu as="div" className="relative flex-none">
+          <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50">
+            <span className="sr-only">options</span>
+            <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
+          </Menu.Button>
+          <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
+            <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-gray-700 py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+              <Menu.Item>
+                <Link to={``} className='block px-3 py-1 text-sm leading-6 text-gray-900 font-semibold hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-500'>See details</Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to={``} className='block px-3 py-1 text-sm leading-6 text-gray-900 font-semibold hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-500'>Edit</Link>
+              </Menu.Item>
+              <Menu.Item>
+                <button type='button' className='block px-3 py-1 text-sm leading-6 text-red-500 font-semibold dark:text-red-400 ' onClick={() => {} }>Delete</button>
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </div>
+    </li>
+  )
+}
---
+++ client/src/components/Card/ProjectCardContainer.tsx
@@ -0,0 +1,13 @@
+import { ProjectCardContainerProps } from 'typescript/interfaces/Project'
+import ProjectCard from './ProjectCard'
+
+
+export default function ProjectCardContainer({data}:ProjectCardContainerProps) {
+  return (
+    <ul role="list" className="divide-y divide-gray-100 border border-gray-100 mt-10 bg-white shadow-lg rounded-md
+    dark:bg-slate-900 dark:divide-gray-600 dark:border-gray-600 
+    ">
+    {data.map((project) => (<ProjectCard project={project}/>))}
+    </ul>
+  )
+}
