bc0ee6fc8d875d71e491f1d11a14ad9cd3034295
José Brito <josealbritose@gmail.com>
2024-04-10 18:39:04 -0300

    EditProjectView

---
+++ client/src/views/projects/EditProjectView.tsx
@@ -0,0 +1,31 @@
+import { Navigate, useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query";
+import { getProject } from "../../server/projectAPI";
+import EditProjectForm from "@/components/form/EditProjectForm";
+
+export default function EditProjectView() {
+  const params = useParams();
+  const projectId = params.projectId!
+
+  const { data,isError,isLoading } = useQuery({
+    queryKey:['project',projectId],
+    queryFn:()=>getProject(projectId),
+    retry:false
+  })
+
+  if(isError){
+    <Navigate to={'/404'}></Navigate>
+  }
+
+  if(isLoading){
+    return (<p>Loading...</p>)
+  }
+
+  console.log(data)
+
+  return (
+    <>
+      {data && <EditProjectForm data={data}/>}
+    </>
+  )
+}
