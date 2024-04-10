5c3ba9c54d0741b3021027c1075685828ef975be
José Brito <josealbritose@gmail.com>
2024-04-10 19:50:29 -0300

    Logo

--- client/src/components/shared/Header.tsx
+++ client/src/components/shared/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import NavMenu from "./NavMenu";
 
@@ -6,7 +7,9 @@ export default function Header() {
     <header className="bg-gray-800 dark:bg-slate-950">
       <div className="max-w-screen-2xl mx-auto flex flex-row justify-between pl-5 pr-16 lg:px-0 items-center pb-5 lg:pb-0">
         <div className="w-64">
-          <Logo/>
+          <Link to={'/'}>
+            <Logo/>
+          </Link>
         </div>
         <NavMenu/>
       </div>
