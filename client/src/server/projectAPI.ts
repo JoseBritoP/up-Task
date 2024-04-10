07f2f7f00f805b2fad2a492c43c11ab8e79fad91
José Brito <josealbritose@gmail.com>
2024-04-10 13:59:34 -0300

    endpoints

---
+++ client/src/server/projectAPI.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import api from "../lib/axios"
+import { ProjectFormData } from "typescript/types/Project";
+
+export async function createProject(formData:ProjectFormData){
+  console.log(formData)
+  try {
+    const { data } = await api.post('/project',formData);
+    console.log(data);
+  } catch (error:any) {
+    console.log(error);
+  }
+}
