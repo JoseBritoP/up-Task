029f2b3fff3b6492276784f42b7b168e177fc6ee
José Brito <josealbritose@gmail.com>
2024-04-10 13:34:46 -0300

    DashboardView

--- client/src/views/DashboardView.tsx
+++ client/src/views/DashboardView.tsx
@@ -1,15 +1,19 @@
+import FormComponent from "@/components/form/FormComponent"
 import { Link } from "react-router-dom"
 
 export default function DashboardView() {
   return (
     <>
-      <h1 className='text-4xl lg:text-5xl font-bold'>My projects</h1>
-      <p className=' text-2xl lg:text-3xl font-semibold text-gray-600 dark:text-gray-400 mt-5'>Manage your projects</p>
-      <nav className="my-8">
-        <Link to={'/projects/create'} className="bg-purple-700 hover:bg-purple-600 
-        dark:bg-purple-800 dark:hover:bg-purple-700
-        px-10 py-3 text-white text-xl font-semibold cursor-pointer transition-colors rounded-md">New Project</Link>
-      </nav>
+      <div className="max-w-3xl mx-auto">
+        <h1 className='text-4xl lg:text-5xl font-bold'>My projects</h1>
+        <p className=' text-2xl lg:text-3xl font-semibold text-gray-600 dark:text-gray-400 mt-5'>Manage your projects</p>
+        <nav className="my-8">
+          <Link to={'/projects/create'} className="bg-purple-700 hover:bg-purple-600 
+          dark:bg-purple-800 dark:hover:bg-purple-700
+          px-10 py-3 text-white text-xl font-semibold cursor-pointer transition-colors rounded-md">New Project</Link>
+        </nav>
+        <FormComponent/>
+      </div>
     </>
   )
 }
