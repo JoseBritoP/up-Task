d89a7089e219f5a68900d7e3e64c661357ec0030
José Brito <josealbritose@gmail.com>
2024-04-10 16:53:15 -0300

    endpoints

--- client/src/server/projectAPI.ts
+++ client/src/server/projectAPI.ts
@@ -2,6 +2,7 @@
 import { isAxiosError } from "axios";
 import api from "../lib/axios"
 import { ProjectFormData } from "typescript/types/Project";
+import { projectsSchema } from "../schema/ProjectSchema";
 
 export async function createProject(formData:ProjectFormData){
   try {
@@ -14,5 +15,22 @@ export async function createProject(formData:ProjectFormData){
       const errorMessage = error.response.data.error.issues.map((issue:{message:'string'})=>issue.message)
       throw new Error(errorMessage);
     }
+  }
+}
+
+export async function getProjects() {
+  try {
+    const { data } = await api.get('/project');
+    const result = projectsSchema.safeParse(data);
+    if(!result.success){
+      console.log('error...',result.error)
+      return
+    }
+    return result.data;
+  } catch (error:any) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error);
+    }
+    throw new Error(`Error fetching projects...`)
   }
 }
