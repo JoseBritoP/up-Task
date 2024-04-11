d7e8fa75bae3ff04a6ae3f088de6a255bb5304d8
José Brito <josealbritose@gmail.com>
2024-04-11 20:10:29 -0300

    authSchema

---
+++ api/src/schema/auth.ts
@@ -0,0 +1,18 @@
+import z from 'zod'
+
+const name = z.string().min(3,{message:'The name is so short'}).max(10,{message:'The name is so long'});
+const email= z.string().email({message:'Email invalid'});
+const password = z.string().min(3,{message:'The password is so short'}).max(20,{message:'The password is so long'});
+const repeatPassword = z.string().min(3,{message:'The repeat password is so short'}).max(20,{message:'The repeat password is so long'});
+const confirmed = z.boolean().default(false);
+
+export const authSchema = z.object({
+  name,
+  email,
+  password,
+  repeatPassword,
+  confirmed
+})
+
+
+export type AuthAccount = z.infer<typeof authSchema>
