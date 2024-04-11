f2a9ac93845c4ab1439fd652a9bd362d66fc84c5
José Brito <josealbritose@gmail.com>
2024-04-11 20:10:14 -0300

    createAccount middleware

---
+++ api/src/middleware/auth.ts
@@ -0,0 +1,21 @@
+import type { Request,Response,NextFunction } from "express";
+import { AuthAccount, authSchema } from "../schema/auth";
+declare global {
+  namespace Express {
+    interface Request {
+      body:AuthAccount
+    }
+  }
+}
+
+export const createAccountMiddleware = (req:Request,res:Response,next:NextFunction) => {
+  try {
+    const body = req.body;
+    const result = authSchema.safeParse(body);
+    if(!result.success) throw new Error(JSON.stringify(result.error))
+    req.body = result.data
+    next();
+  } catch (error:any) {
+    return res.status(400).json({error:JSON.parse(error.message)})
+  }
+};
