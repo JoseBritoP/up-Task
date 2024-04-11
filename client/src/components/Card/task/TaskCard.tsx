dc65a34ad6bd5f653f2192f634a25e7f6218874b
José Brito <josealbritose@gmail.com>
2024-04-10 23:39:40 -0300

    TaskCard

---
+++ client/src/components/Card/task/TaskCard.tsx
@@ -0,0 +1,41 @@
+import { Fragment } from 'react'
+import { Task } from '../../../schema/TaskSchema'
+import { Menu, Transition } from '@headlessui/react'
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
+
+interface TaskCardProps {
+  task:Task
+}
+export default function TaskCard({task}:TaskCardProps) {
+  return (
+    <li className='p-5 bg-white dark:bg-gray-700/60 border-2 border-slate-300 dark:border-slate-600 flex justify-between gap-3 rounded-md'>
+      <div className='min-w-0 flex flex-col gap-y-4'>
+        <button className='text-xl font-semibold text-slate-600 dark:text-slate-300 text-left'>{task.name}</button>
+        <p className='text-slate-500 dark:text-slate-300'>{task.description}</p>
+      </div>
+      <div className="flex shrink-0  gap-x-6">
+        <Menu as="div" className="relative flex-none">
+          <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">
+            <span className="sr-only">options</span>
+            <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
+          </Menu.Button>
+            <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
+              <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-gray-700 py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                <Menu.Item>
+                  <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900 dark:text-gray-300 '> See Task</button>
+                </Menu.Item>
+                <Menu.Item>
+                  <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900 dark:text-gray-300'>Edit Task</button>
+                </Menu.Item>
+                <Menu.Item>
+                  <button type='button' className='block px-3 py-1 text-sm leading-6 text-red-500 font-semibold'>Delete Task</button>
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+        </Menu>
+      </div>
+    </li>
+  )
+}
---
+++ client/src/components/Card/task/TaskList.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Task } from '../../../schema/TaskSchema'
+import TaskCard from './TaskCard'
+
+interface TaskListProps{
+  tasks:Task[]
+}
+
+type GroupedTask = {
+  [key:string]:Task[]
+}
+const initialStatusGroup:GroupedTask = {
+  pending:[],
+  onHold:[],
+  inProgress:[],
+  underReview:[],
+  completed:[]
+}
+
+const statusTranslations:{[key:string]:string} = {
+  pending:'Pending',
+  onHold:'On Hold',
+  inProgress: 'In Progress',
+  underReview: 'Under Review',
+  completed: 'Completed'
+}
+
+const statusStyles:{[key:string]:string} = {
+  pending:'border-t-slate-400 dark:border-t-slate-500',
+  onHold:'border-t-red-400 dark:border-t-red-500',
+  inProgress: 'border-t-blue-400 dark:border-t-blue-500',
+  underReview: 'border-t-amber-400 dark:border-t-amber-500',
+  completed: 'border-t-emerald-400 dark:border-t-emerald-500'
+}
+export default function TaskList({tasks}:TaskListProps) {
+
+
+  const groupedTasks = tasks.reduce((acc, task) => {
+    let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
+    currentGroup = [...currentGroup, task]
+    return { ...acc, [task.status]: currentGroup };
+}, initialStatusGroup);
+
+  return (
+    <>
+      <h2 className="text-3xl font-bold my-10">Tareas</h2>
+      <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
+        {Object.entries(groupedTasks).map(([status, tasks]) => (
+          <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+            <h3 className={` capitalize text-xl font-semibold border border-slate-300 bg-white p-3 border-t-8 dark:bg-gray-700 dark:border-slate-600 rounded-t-none rounded-md ${statusStyles[status]}`}>{statusTranslations[status]}</h3>
+            <ul className='mt-5 space-y-5'>
+              {tasks.length === 0 ? (
+                  <li className="text-gray-500 text-center pt-3">No task</li>
+              ) : (
+                  tasks.map(task => <TaskCard key={task._id} task={task} />)
+              )}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
