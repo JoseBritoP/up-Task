be1f102ca026499f7de0945730b9c93fba02ce7e
José Brito <josealbritose@gmail.com>
2024-04-11 21:27:34 -0300

    createAccountController

--- api/src/controllers/auth/01 - createAccount.ts
+++ api/src/controllers/auth/01 - createAccount.ts
@@ -3,7 +3,7 @@ import User from "../../models/User";
 import { AuthAccount } from "../../schema/auth";
 import { hashPassword } from "../../utils/auth";
 import { generateToken } from "../../utils/token";
-
+import { AuthEmail } from "../../emails/AuthEmail";
 const existEmail = async (email:string) => {
   const user = await User.findOne({email});
   if(user) throw new Error(`The email is already used`)
@@ -18,8 +18,8 @@ export const createAccount = async (data:AuthAccount) => {
   const token = new Token();
   token.token = generateToken();
   token.user = newAccount.id
-  
-
+  AuthEmail.sendConfirmationEmail({email:newAccount.email,name:newAccount.name,token:token.token})
+ 
   const [account,newToken] = await Promise.allSettled([newAccount.save(),token.save()])
   return {
     message:'Check your email to confirm your account',
