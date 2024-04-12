3fa7eff20eeb16d19f09ccf50d9947d6178eff8e
José Brito <josealbritose@gmail.com>
2024-04-12 17:34:25 -0300

    Fix link

--- api/src/emails/AuthEmail.ts
+++ api/src/emails/AuthEmail.ts
@@ -30,7 +30,7 @@ const sendChangePassword = async ({email,name,token}:Email) => {
     text:'UpTask - Reset Password',
     html:`<p>Hello ${name}, you seem to have forgotten your password</p>
     <p>Please visit the following link to reset it:</p>
-    <a href="${process.env.FRONTEND_URL}/auth/forget-password">Confirm Account</a>
+    <a href="${process.env.FRONTEND_URL}/auth/new-password">Confirm Account</a>
     <p>:and enter the following token: <b>${token}</b></p>
     <p>This token expires in 10 min</p>
     `
