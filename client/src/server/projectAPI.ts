a609ce5f833178985932df231b4572e6a98884e7
José Brito <josealbritose@gmail.com>
2024-04-13 01:22:40 -0300

    Axios instance config headers

--- client/src/server/projectAPI.ts
+++ client/src/server/projectAPI.ts
@@ -9,8 +9,6 @@ export async function createProject(formData:ProjectFormData){
     const { data } = await api.post('/project',formData);
     return data
   } catch (error:any) {
-    // console.log('Server error')
-    // console.log(error);
     if(isAxiosError(error) && error.response){
       const errorMessage = error.response.data.error.issues.map((issue:{message:'string'})=>issue.message)
       throw new Error(errorMessage);
