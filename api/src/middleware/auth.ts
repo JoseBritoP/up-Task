6f44d747a4020eec7e5e030e35c36427d32f07f6
José Brito <josealbritose@gmail.com>
2024-04-12 23:27:31 -0300

    Authenticate middleware

--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -44,11 +44,9 @@ export const authenticate = async (req:Request,res:Response,next:NextFunction) =
   if(!req.headers.authorization === undefined || req.headers.authorization?.split(' ')[1] === undefined) throw new Error(`Unauthorized`)
     const token = req.headers.authorization?.split(' ')[1]
 
-    console.log(token);
     const decoded = jwt.verify(token,process.env.JWT_SECRET!)
-    console.log(decoded)
     if(typeof decoded === 'object' && decoded.id){
-      const user = await User.findById(decoded.id)
+      const user = await User.findById(decoded.id).select('_id name email')
       req.user = user
       if(!user) throw new Error(`Invalid token`)
     }
