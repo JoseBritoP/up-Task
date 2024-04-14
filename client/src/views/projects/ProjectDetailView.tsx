44228871058fafa2698f2d71afdc0d54ecae83b8
José Brito <josealbritose@gmail.com>
2024-04-14 19:01:09 -0300

    BackToProjects Link

--- client/src/views/projects/ProjectDetailView.tsx
+++ client/src/views/projects/ProjectDetailView.tsx
@@ -34,8 +34,9 @@ export default function ProjectDetailView() {
       <h1 className='text-4xl font-bold uppercase'>{data?.projectName}</h1>
       <p className='text-2xl font-light text-gray-500 dark:text-gray-400 mt-5'>{data?.description}</p>
       <nav className='my-5 flex gap-3'>
-        <button className='bg-sky-500 hover:bg-sky-600 dark:bg-sky-700 dark:hover:bg-sky-600 px-6 py-2 text-white text-xl font-semibold cursor-pointer transition-colors rounded-md text-center' onClick={()=>navigate(location.pathname + `?newTask=true`)}>Add task</button>
+        <button className='bg-sky-600 hover:bg-sky-500 dark:bg-sky-700 dark:hover:bg-sky-600 px-6 py-2 text-white text-xl font-semibold cursor-pointer transition-colors rounded-md text-center' onClick={()=>navigate(location.pathname + `?newTask=true`)}>Add task</button>
         <Link to={'team'} className='bg-violet-500 hover:bg-violet-600 dark:bg-violet-700 dark:hover:bg-violet-600 px-6 py-2 text-white text-xl font-semibold cursor-pointer transition-colors rounded-md text-center'>Partners</Link>
+        <Link to={'/projects'} className='bg-slate-600 hover:bg-slate-500 dark:bg-cyan-700 dark:hover:bg-cyan-600 px-6 py-2 text-white text-xl font-semibold cursor-pointer transition-colors rounded-md text-center'>Back to Projects</Link>
       </nav>
       {data && data.tasks && <TaskList tasks={data.tasks}/>}
       <AddTaskModal/>
