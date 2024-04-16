ef4385ba45cd3e4a579d310d19d1f349540c4393
José Brito <josealbritose@gmail.com>
2024-04-16 15:53:31 -0300

    ProfileSection

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -5,6 +5,7 @@ import DashboardView from "./views/DashboardView";
 import { AuthView, ConfirmAccount, NewPassword, ResetPassword,RequestNewCode } from "./views/auth";
 import { CreateProjectView, EditProjectView, ProjectDetailView, ProjectTeamView } from "./views/projects";
 import { ChangePasswordView, ProfileView } from "./views/profile";
+import ProfileLayout from "./layouts/ProfileLayout";
 
 export default function Router(){
   return (
@@ -16,8 +17,10 @@ export default function Router(){
           <Route path="/projects/:projectId" element={<ProjectDetailView/>}/>
           <Route path="/projects/:projectId/edit" element={<EditProjectView/>}/>
           <Route path="/projects/:projectId/team" element={<ProjectTeamView/>}/>
-          <Route path="/profile" element={<ProfileView/>}/>
-          <Route path="/profile/password" element={<ChangePasswordView/>}/>
+          <Route element={<ProfileLayout/>}>
+            <Route path="/profile" element={<ProfileView/>}/>
+            <Route path="/profile/password" element={<ChangePasswordView/>}/>
+          </Route>
         </Route>
         <Route element={<AuthLayout/>}>
           <Route path="/auth" element={<AuthView/>}/>
