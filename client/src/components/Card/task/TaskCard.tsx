910dcdf77902a541c65d9319b329087357c0479d
José Brito <josealbritose@gmail.com>
2024-04-16 20:00:13 -0300

    DragTask

--- client/src/components/Card/task/TaskCard.tsx
+++ client/src/components/Card/task/TaskCard.tsx
@@ -1,42 +1,64 @@
-import { Fragment } from 'react'
-import { Task } from '../../../schema/TaskSchema'
-import { Menu, Transition } from '@headlessui/react'
-import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
-import { useNavigate, useParams } from 'react-router-dom'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { deleteTask } from '@/server/taskAPI'
-import { toast } from 'react-toastify'
+import { Fragment } from "react";
+import { Task } from "../../../schema/TaskSchema";
+import { Menu, Transition } from "@headlessui/react";
+import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
+import { useNavigate, useParams } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteTask } from "@/server/taskAPI";
+import { toast } from "react-toastify";
+import { useDraggable } from "@dnd-kit/core";
 
 interface TaskCardProps {
-  task:Task,
-  userId:string,
-  canEdit:boolean
+  task: Task;
+  userId: string;
+  canEdit: boolean;
 }
-export default function TaskCard({task,userId,canEdit}:TaskCardProps) {
 
+export default function TaskCard({ task, userId, canEdit }: TaskCardProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: task._id,
+  });
 
   const params = useParams();
-  const projectId = params.projectId!
+  const projectId = params.projectId!;
   const navigate = useNavigate();
 
   const queryClient = useQueryClient();
 
   const { mutate } = useMutation({
-    mutationFn:deleteTask,
-    onError:(error)=>{
-      if(error.message) return toast.error(error.message)
-      toast.error(`Error deleting the task`)
+    mutationFn: deleteTask,
+    onError: (error) => {
+      if (error.message) return toast.error(error.message);
+      toast.error(`Error deleting the task`);
     },
-    onSuccess:()=>{
-      queryClient.invalidateQueries({queryKey:['project',projectId]})
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["project", projectId] });
       toast.success(`Task was deleted successfully`);
-    }
-  })
+    },
+  });
+
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px,${transform.y}px,0)`,
+      }
+    : undefined;
+
   return (
-    <li className='p-5 bg-white dark:bg-gray-700/60 border-2 border-slate-300 dark:border-slate-600 flex justify-between gap-3 rounded-md'>
-      <div className='min-w-0 flex flex-col gap-y-4'>
-        <button className='text-xl font-semibold text-slate-600 dark:text-slate-300 text-left' onClick={()=>navigate(location.pathname+`?viewTask=${task._id}`)}>{task.name}</button>
-        <p className='text-slate-500 dark:text-slate-300'>{task.description}</p>
+    <li className="p-5 bg-white dark:bg-gray-700/60 border-2 border-slate-300 dark:border-slate-600 flex justify-between gap-3 rounded-md">
+      <div
+        className="min-w-0 flex flex-col gap-y-4"
+        {...listeners}
+        {...attributes}
+        ref={setNodeRef}
+        style={style}
+      >
+        <button
+          className="text-xl font-semibold text-slate-600 dark:text-slate-300 text-left"
+          onClick={() => navigate(location.pathname + `?viewTask=${task._id}`)}
+        >
+          {task.name}
+        </button>
+        <p className="text-slate-500 dark:text-slate-300">{task.description}</p>
       </div>
       <div className="flex shrink-0  gap-x-6">
         <Menu as="div" className="relative flex-none">
@@ -44,31 +66,56 @@ export default function TaskCard({task,userId,canEdit}:TaskCardProps) {
             <span className="sr-only">options</span>
             <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
           </Menu.Button>
-            <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
-              <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-gray-700 py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                <Menu.Item>
-                  <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900 dark:text-gray-300' 
-                  onClick={()=>navigate(location.pathname+`?viewTask=${task._id}`)}> See Task</button>
-                </Menu.Item>
-                {canEdit && (
-                  <>
-                <Menu.Item>
-                  <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900 dark:text-gray-300'
-                  onClick={()=>navigate(location.pathname+`?editTask=${task._id}`)} >Edit Task</button>
-                </Menu.Item>
-                <Menu.Item>
-                  <button type='button' className='block px-3 py-1 text-sm leading-6 text-red-500 font-semibold'
-                    onClick={()=>mutate({taskId:task._id,userId})}
-                    >Delete Task</button>
-                </Menu.Item>
-                    </>
-                  )}
-              </Menu.Items>
-            </Transition>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-gray-700 py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+              <Menu.Item>
+                <button
+                  type="button"
+                  className="block px-3 py-1 text-sm leading-6 text-gray-900 dark:text-gray-300"
+                  onClick={() =>
+                    navigate(location.pathname + `?viewTask=${task._id}`)
+                  }
+                >
+                  {" "}
+                  See Task
+                </button>
+              </Menu.Item>
+              {canEdit && (
+                <>
+                  <Menu.Item>
+                    <button
+                      type="button"
+                      className="block px-3 py-1 text-sm leading-6 text-gray-900 dark:text-gray-300"
+                      onClick={() =>
+                        navigate(location.pathname + `?editTask=${task._id}`)
+                      }
+                    >
+                      Edit Task
+                    </button>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <button
+                      type="button"
+                      className="block px-3 py-1 text-sm leading-6 text-red-500 font-semibold"
+                      onClick={() => mutate({ taskId: task._id, userId })}
+                    >
+                      Delete Task
+                    </button>
+                  </Menu.Item>
+                </>
+              )}
+            </Menu.Items>
+          </Transition>
         </Menu>
       </div>
     </li>
-  )
+  );
 }
--- client/src/components/Card/task/TaskListContainer.tsx
+++ client/src/components/Card/task/TaskListContainer.tsx
@@ -1,58 +1,126 @@
-import { statusTranslations } from '@/locales/qs'
-import { Task } from '../../../schema/TaskSchema'
-import TaskCard from './TaskCard'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { statusTranslations } from "@/locales/qs";
+import { Task, TaskStatus } from "../../../schema/TaskSchema";
+import TaskCard from "./TaskCard";
+import DropTask from "@/components/shared/task/DropTask";
+import { DndContext,DragEndEvent } from "@dnd-kit/core";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { taskStatus } from "@/server/taskAPI";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
+import useAuth from "@/hooks/auth/useAuth";
 
-interface TaskListProps{
-  tasks:Task[],
-  userId:string,
-  canEdit:boolean
+interface TaskListProps {
+  tasks: Task[];
+  userId: string;
+  canEdit: boolean;
 }
 
 type GroupedTask = {
-  [key:string]:Task[]
-}
-const initialStatusGroup:GroupedTask = {
-  pending:[],
-  onHold:[],
-  inProgress:[],
-  underReview:[],
-  completed:[]
-}
+  [key: string]: Task[];
+};
+const initialStatusGroup: GroupedTask = {
+  pending: [],
+  onHold: [],
+  inProgress: [],
+  underReview: [],
+  completed: [],
+};
+
+const statusStyles: { [key: string]: string } = {
+  pending: "border-t-slate-400 dark:border-t-slate-500",
+  onHold: "border-t-red-400 dark:border-t-red-500",
+  inProgress: "border-t-blue-400 dark:border-t-blue-500",
+  underReview: "border-t-amber-400 dark:border-t-amber-500",
+  completed: "border-t-emerald-400 dark:border-t-emerald-500",
+};
 
 
-const statusStyles:{[key:string]:string} = {
-  pending:'border-t-slate-400 dark:border-t-slate-500',
-  onHold:'border-t-red-400 dark:border-t-red-500',
-  inProgress: 'border-t-blue-400 dark:border-t-blue-500',
-  underReview: 'border-t-amber-400 dark:border-t-amber-500',
-  completed: 'border-t-emerald-400 dark:border-t-emerald-500'
-}
-export default function TaskListContainer({tasks,userId,canEdit}:TaskListProps) {
-
-
+export default function TaskListContainer({tasks,userId,canEdit,}: TaskListProps) {
+  
+  const { data:profile } = useAuth();
+  
   const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
-    currentGroup = [...currentGroup, task]
+    currentGroup = [...currentGroup, task];
     return { ...acc, [task.status]: currentGroup };
-}, initialStatusGroup);
+  }, initialStatusGroup);
 
+
+  const params = useParams();
+  const projectId = params.projectId!
+
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn:taskStatus,
+    onError:(error)=>{
+      toast.error(error.message)
+    },
+    onSuccess:(data)=>{
+      toast.success(data.message)
+      queryClient.invalidateQueries({queryKey:['project',projectId]})
+    }
+  });
+
+  const handleDragEnd = (e:DragEndEvent) => {
+    const { over, active } = e;
+
+    if(over && over.id){
+      console.log('valid');
+      const taskId = active.id.toString()
+      const status = over.id as TaskStatus
+
+      mutate({taskId,status,userId:profile && profile._id})
+
+      queryClient.setQueryData(['project',projectId],(oldData:any)=>{
+        const updatedTask = oldData.tasks.map((task:Task)=>{
+          if(task._id === taskId){
+            return {
+              ...task,status
+            }
+          }
+          return task
+        })
+
+        return {
+          ...oldData,
+          tasks:updatedTask
+        }
+      })
+    }
+  };
   return (
     <>
-      <h2 className="text-3xl font-bold my-10">Tareas</h2>
-      <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
-        {Object.entries(groupedTasks).map(([status, tasks]) => (
-          <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
-            <h3 className={` capitalize text-xl font-semibold border border-slate-300 bg-white p-3 border-t-8 dark:bg-gray-700 dark:border-slate-600 rounded-t-none rounded-md ${statusStyles[status]}`}>{statusTranslations[status]}</h3>
-            <ul className='mt-5 space-y-5'>
-              {tasks.length === 0 ? (
+      <h2 className="text-3xl font-bold my-10">Tasks</h2>
+      <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
+        <DndContext onDragEnd={handleDragEnd}>
+          {Object.entries(groupedTasks).map(([status, tasks]) => (
+            <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
+              <h3
+                className={` capitalize text-xl font-semibold border border-slate-300 bg-white p-3 border-t-8 dark:bg-gray-700 dark:border-slate-600 rounded-t-none rounded-md ${statusStyles[status]}`}
+              >
+                {statusTranslations[status]}
+              </h3>
+              <DropTask status={status}/>
+              <ul className="mt-5 space-y-5">
+                {tasks.length === 0 ? (
                   <li className="text-gray-500 text-center pt-3">No task</li>
-              ) : (
-                  tasks.map(task => <TaskCard key={task._id} task={task} userId={userId} canEdit={canEdit}/>)
-              )}
-            </ul>
-          </div>
-        ))}
+                ) : (
+                  tasks.map((task) => (
+                    <TaskCard
+                      key={task._id}
+                      task={task}
+                      userId={userId}
+                      canEdit={canEdit}
+                    />
+                  ))
+                )}
+              </ul>
+            </div>
+          ))}
+        </DndContext>
       </div>
     </>
-  )
+  );
 }
