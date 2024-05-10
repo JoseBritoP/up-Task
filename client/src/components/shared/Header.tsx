64c5240f1d58b5d1e299bae4d5bac7e1288a1628
José Brito <josealbritose@gmail.com>
2024-05-10 10:21:23 -0300

    Styles

--- client/src/components/shared/Header.tsx
+++ client/src/components/shared/Header.tsx
@@ -11,13 +11,13 @@ interface HeaderProps {
 export default function Header({user}:HeaderProps) {
   return (
     <header className="bg-gray-800 dark:bg-slate-950">
-      <div className="max-w-screen-2xl mx-auto flex flex-row justify-between pl-5 pr-16 lg:px-0 items-center pb-5 lg:pb-0">
+      <div className="max-w-screen-2xl mx-auto flex flex-row justify-between pl-1 md:pl-5 pr-16 lg:px-0 items-center pb-5 lg:pb-0">
         <div className="w-64">
           <Link to={'/projects'}>
             <Logo/>
           </Link>
         </div>
-        <div className="flex justify-between items-center gap-x-5 pr-4">
+        <div className="flex justify-between items-center gap-x-5 pr-1 md:pr-4">
           <DarkMode/>
           <NavMenu user={user}/>
         </div>
